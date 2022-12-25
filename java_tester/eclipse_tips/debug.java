04d8308bfccc800e30c7aaaf0d24a8a3f905cba1
nguyenha <nguyentthuha1999@gmail.com>
2022-12-25 16:22:32 +0700

    eclipse_tips_done

--- java_tester/eclipse_tips/debug.java
+++ java_tester/eclipse_tips/debug.java
@@ -9,23 +9,25 @@ import org.testng.annotations.Test;
 public class debug {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+
 	String osName = System.getProperty("os.name");
 
 	@Test
 	public void tc_01() {
-		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-		driver = new ChromeDriver(); 
-		
+		System.out.println(projectPath);
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browser_drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+
 		driver.get("https://www.facebook.com/");
-		
+
 		String headerText = driver.findElement(By.xpath("//div[@id='content']//h2")).getText();
-		//System.out.println(headerText); -> Trong nhiều dự án không cho in dòng này ra, mà chỉ cho debug thôi
+		// System.out.println(headerText); -> Trong nhiều dự án không cho in dòng này ra, mà chỉ cho debug thôi
 		Assert.assertEquals(headerText, "Facebook helps you connect and share with the people in your life.");
-		
+
 		boolean loginStatus = driver.findElement(By.name("login")).isDisplayed();
 		Assert.assertTrue(loginStatus);
-		
-		//Khi run thì không dùng Run As mà dùng Debug As
+
+		// Khi run thì không dùng Run As mà dùng Debug As
 		// Sau khi debug1 chạy -> ấn Resume thì nó sẽ chạy đến điểm debug tiếp theo rồi dừng
 		// Mục đích để dùng xem được từng bước nó chạy kĩ hơn, hiểu luồng dữ liệu chạy lần lượt ntn
 		// Khi làm đủ nhiều thì k cần dùng debug nhiều, vì có thể tự đoán ra được lỗi và fix lỗi được mà k cần dùng debug
