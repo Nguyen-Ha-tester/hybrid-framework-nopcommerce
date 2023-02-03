ecbfe1cda509bd26e8862af6d6f2e6a3c3fa1ad8
nguyenha <nguyentthuha1999@gmail.com>
2023-02-03 22:20:46 +0700

    update

--- interfaces/pageUIs/nopCommerce/user/UserBasePageUI.java
+++ interfaces/pageUIs/nopCommerce/user/UserBasePageUI.java
@@ -6,6 +6,10 @@ public class UserBasePageUI {
 	public static final String ORDERS_LINK = "xpath=//div[@class='block block-account-navigation']//a[text()='Orders']";
 	public static final String REWARD_POINTS_LINK = "xpath=//div[@class='block block-account-navigation']//a[text()='Reward points']";
 	public static final String LOG_OUT_LINK = "class=ico-logout";
+
+	// Dùng dynamic-dùng theo pattern object.
 	public static final String DYNAMIC_PAGES_IN_MY_ACCOUNT_PAGE = "xpath=//div[@class='block block-account-navigation']//a[text()='%s']";
+	public static final String DYNAMIC_TEXTBOX_BY_ID = "xpath=//input[@id='%s']";
+	public static final String DYNAMIC_LOGIN_BUTTON_BY_TEXT = null;
 
 }
