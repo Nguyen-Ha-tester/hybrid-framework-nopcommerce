8d7694ac4a4f7a25e29cb3ca78b4c0c94c886edf
nguyenha <nguyentthuha1999@gmail.com>
2023-02-24 21:42:57 +0700

    web_driver_manager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,8 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	private WebDriver driver;
@@ -13,28 +18,63 @@ public class BaseTest {
 	private String projectPath = System.getProperty("user.dir");
 
 	protected WebDriver getBrowserDriver(String browserName) {
+		// Firefox
 		if (browserName.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
 			if (osName.contains("Mac OS")) {
-				System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 			} else {
-				System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+				WebDriverManager.firefoxdriver().setup();
 			}
 			driver = new FirefoxDriver();
+		} else if (browserName.equals("h_firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size: 1920x1080");
+			driver = new FirefoxDriver();
+
+			// Chrome
 		} else if (browserName.equals("chrome")) {
 			if (osName.contains("Mac OS")) {
-				System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
-
+				WebDriverManager.chromedriver().setup();
 			} else {
-				System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+				WebDriverManager.chromedriver().setup();
 			}
 			driver = new ChromeDriver();
+		} else if (browserName.equals("h_chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size: 1920x1080");
+			driver = new ChromeDriver();
+
+			// Edge
 		} else if (browserName.equals("edge")) {
 			if (osName.contains("Mac OS")) {
-				System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+				WebDriverManager.edgedriver().setup();
 			} else {
-				System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+				WebDriverManager.edgedriver().setup();
 			}
 			driver = new EdgeDriver();
+
+			// Opera
+		} else if (browserName.equals("opera")) {
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+
+			// Coc Coc: use chromedriver version older 5 or 6 than cococ version
+		} else if (browserName.equals("coc coc")) {
+			WebDriverManager.chromedriver().driverVersion("107.0.5304.62").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("");
+			driver = new ChromeDriver(options);
+
+			// Brave: use chromedriver version same with brave browser version
+		} else if (browserName.equals("brave")) {
+			WebDriverManager.chromedriver().driverVersion(".....").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("");
+			driver = new ChromeDriver(options);
 		} else {
 			throw new RuntimeException("Please input with correct browser name."); // RuntimeException có nghĩa là chạy lỗi phát là throw ngay
 		}
