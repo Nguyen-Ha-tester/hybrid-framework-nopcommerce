ef1b528c6d25c655a4cf58d87532047b1e904a2d
Ha Nguyen <106964292+hanguyen10061999@users.noreply.github.com>
2023-03-21 18:08:00 +0700

    update code

--- javatester/javaBasic/topic_12_substring.java
+++ javatester/javaBasic/topic_12_substring.java
@@ -4,7 +4,7 @@ public class topic_12_substring {
 	public static void main(String[] args) {
 		String firstText = "id=Email";
 		String secondText = "css=input[id='Password']";
-		String thirdText = "xpath=//button[@text()='Log in']";
+		String thirdText = "xpath=button[@text()='Log in']";
 
 		System.out.println(firstText.substring(3));
 		System.out.println(secondText.substring(4));
