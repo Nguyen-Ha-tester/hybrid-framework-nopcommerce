6c036c2739c1d493068ac6d3d1cc9d5ea866e485
Ha Nguyen <106964292+hanguyen10061999@users.noreply.github.com>
2023-03-23 18:00:57 +0700

    inheritance_encapsulation_in_process

---
+++ javatester/javaOOP/inheritance/Test.java
@@ -0,0 +1,11 @@
+package javaOOP.inheritance;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.equals(test);
+	}
+	// inheritae object class in default
+
+}
