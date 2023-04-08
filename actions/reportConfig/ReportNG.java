24a98c34ba12df7d09ce1178c443e4ad236ecf3f
nguyenha <nguyentthuha1999@gmail.com>
2023-04-08 18:25:00 +0700

    attach_image_in_progress

--- actions/reportConfig/ReportNG.java
+++
@@ -1,5 +0,0 @@
-package reportConfig;
-
-public class ReportNG {
-
-}
---
+++ actions/reportConfig/ReportNGListener.java
@@ -0,0 +1,82 @@
+package reportConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class ReportNGListener implements ITestListener {
+	// ITestContext: đại diện cho testcase
+	// ITestResult: đại diện cho test result
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) { // cần hàm này thui, để biết khi nào fail thì sẽ chụp hình fail lại. Chứ khi success thì thôi k cần show ảnh
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+
+		Object testClass = result.getInstance();
+		WebDriver webDriver = ((AbstractTest) testClass).getDriver();
+
+		String screenshotPath = captureScreenshot(webDriver, result.getName());
+		Reporter.getCurrentTestResult();
+		Reporter.log("<br><a target=\"_blank\" href=\"file:///" + screenshotPath + "\">" + "<img src=\"file:///" + screenshotPath + "\" " + "height='100' width='150'/> " + "</a></br>");
+		Reporter.setCurrentTestResult(null);
+	}
+
+	public String captureScreenshot(WebDriver driver, String screenshotName) {
+		try {
+			Calendar calendar = Calendar.getInstance();
+			SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+			File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			String screenPath = System.getProperty("user.dir") + "\\ReportNGScreenShots\\" + screenshotName + "_" + formater.format(calendar.getTime()) + ".png";
+			FileUtils.copyFile(source, new File(screenPath));
+			return screenPath;
+		} catch (IOException e) {
+			System.out.println("Exception while taking screenshot: " + e.getMessage());
+			return e.getMessage();
+		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
