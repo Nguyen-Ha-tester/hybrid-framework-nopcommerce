ac9e09804616dbd1986804ca7c09b6bfc2114979
Nguyen Thi Thu Ha <nguyentthuha1999@gmail.com>
2023-04-21 17:27:40 +0700

    update

--- interfaces/pageUIs/nopCommerce/user/UserBasePageUI.java
+++ interfaces/pageUIs/nopCommerce/user/UserBasePageUI.java
@@ -10,7 +10,12 @@ public class UserBasePageUI {
 	// Dùng dynamic-dùng theo pattern object.
 	public static final String DYNAMIC_PAGES_IN_MY_ACCOUNT_PAGE = "xpath=//div[@class='block block-account-navigation']//a[text()='%s']";
 	public static final String DYNAMIC_TEXTBOX_BY_ID = "xpath=//input[@id='%s']";
-	public static final String DYNAMIC_LOGIN_BUTTON_BY_TEXT = null;
-	public static final String DYNAMIC_DROPDOWN_BY_NAME = null;
+	public static final String DYNAMIC_LOGIN_BUTTON_BY_TEXT = "xpath=//button[@name='%s']";
+	public static final String DYNAMIC_DROPDOWN_BY_NAME = "";
+	public static final String DYNAMIC_RADIO_BUTTON_BY_NAME = "xpath=//input[@name='%s']";
+	public static final String DYNAMIC_CHECKBOX_BY_NAME = "xpath=//input[@name='%s']";
+	public static final String DYNAMIC_TEXTAREA = "xpath=//textarea[@name='%s']";
+	public static final String DYNAMIC_LINK = "xpath=//a[@name='%s']";
+	public static final String DYNAMIC_ERROR_MESSAGE = "xpath=//td[text()='%s']/following-sibling::td/label";
 
 }
